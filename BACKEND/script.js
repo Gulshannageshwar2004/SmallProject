279cef811f34835f6e712a0376e7f996e04e173b
Gulshan <2004gulshanmehra@gmail.com>
2026-06-09 01:49:35 +0530

    Added backend project2

--- BACKEND/script.js
+++ BACKEND/script.js
@@ -1,2 +1,10 @@
-// 
-console.log(process.argv);
+
+// let args = process.argv;
+
+// for(let i=2; i<args.length; i++) {
+//     console.log("hello to", args[i]);
+// }
+
+const math = require("./math");
+
+console.log(math.sum(2, 2));
